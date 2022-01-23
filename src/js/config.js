f670660ff1c4920889f070da1731c5df00f6e8b1
Mihail Dimitrov <misho987m@abv.bg>
2022-01-23 14:47:46 +0200

    Initial Commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '017585b0-cada-447e-b815-2062e6f6990f';
+export const MODAL_CLOSE_SEC = 2.5;
